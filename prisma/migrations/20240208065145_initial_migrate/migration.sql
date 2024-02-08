a13ec59e2a377f52d3000eb6decfc18c9fabf629
Inun572 <helmi.ainun@gmail.com>
2024-02-08 13:55:15 +0700

    fix: reset migrate and adding shop model

--- prisma/migrations/20240208065145_initial_migrate/migration.sql
+++ prisma/migrations/20240208065145_initial_migrate/migration.sql
@@ -57,6 +57,19 @@ CREATE TABLE `PermissionRole` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `shops` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `shops_user_id_key`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `categories` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
@@ -75,7 +88,7 @@ CREATE TABLE `products` (
     `stock` INTEGER NOT NULL,
     `category_id` INTEGER NOT NULL,
     `is_deleted` BOOLEAN NOT NULL DEFAULT false,
-    `user_id` INTEGER NOT NULL,
+    `seller_id` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -124,7 +137,10 @@ CREATE TABLE `orders` (
     `order_number` VARCHAR(191) NOT NULL,
     `cart_id` INTEGER NOT NULL,
     `total_amount` DOUBLE NOT NULL,
+    `is_cancelled` BOOLEAN NOT NULL DEFAULT false,
     `is_paid` BOOLEAN NOT NULL DEFAULT false,
+    `is_delivered` BOOLEAN NOT NULL DEFAULT false,
+    `is_done` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -156,10 +172,13 @@ ALTER TABLE `PermissionRole` ADD CONSTRAINT `PermissionRole_role_id_fkey` FOREIG
 ALTER TABLE `PermissionRole` ADD CONSTRAINT `PermissionRole_permission_id_fkey` FOREIGN KEY (`permission_id`) REFERENCES `permissions`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE `shops` ADD CONSTRAINT `shops_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `products` ADD CONSTRAINT `products_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `products` ADD CONSTRAINT `products_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `products` ADD CONSTRAINT `products_seller_id_fkey` FOREIGN KEY (`seller_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `images` ADD CONSTRAINT `images_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
