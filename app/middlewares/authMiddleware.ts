2f51dfc7db822562046e79ec414641b4c9659ac6
Inun572 <helmi.ainun@gmail.com>
2024-02-10 15:16:42 +0700

    feat: adding token validator

--- app/middlewares/authMiddleware.ts
+++ app/middlewares/authMiddleware.ts
@@ -1,2 +1,37 @@
+import { NextFunction, Request, Response } from 'express';
+import { findToken } from '../services/authServices';
+import { verifyToken } from '../utils/token';
 
+export const validateToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization;
 
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized',
+    });
+  }
+
+  const verifiedToken = await findToken(token);
+
+  if (!verifiedToken) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+
+  const isExpired = verifyToken(token);
+
+  if (!isExpired) {
+    return res.status(401).json({
+      message: 'Token expired',
+    });
+  }
+
+  req.user = verifiedToken.user;
+
+  next();
+};
