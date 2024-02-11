f7a65a4c8625f7533378c19e7ba58a28b3dc4668
Inun572 <helmi.ainun@gmail.com>
2024-02-11 14:38:01 +0700

    feat: services for category

---
+++ app/services/categoryServices.ts
@@ -0,0 +1,44 @@
+import prisma from '../config/db';
+
+export const getAllCategories = async () => {
+  return await prisma.category.findMany({
+    select: {
+      name: true,
+    },
+  });
+};
+
+export const findCategoryById = async (id: number) => {
+  return await prisma.category.findFirst({
+    where: {
+      id,
+    },
+  });
+};
+
+export const addCategory = async (name: string) => {
+  return await prisma.category.create({
+    data: {
+      name,
+    },
+  });
+};
+
+export const updateCategory = async (id: number, name: string) => {
+  return await prisma.category.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+    },
+  });
+};
+
+export const deleteCategory = async (id: number) => {
+  return await prisma.category.delete({
+    where: {
+      id,
+    },
+  });
+};
